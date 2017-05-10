495b0208666199dfe5b05c1069d9c5285bc7c0c1
Carolina <Carolina@MacBook-Pro.local>
2017-05-11 01:16:16 +0200

    register validations checks added

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,28 @@ router.get('/login', (req,res) =>{
   res.render('login');
 })
 
+// Register User
+router.post('/register', (req,res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+
+   // Validation
+   req.checkBody('name', 'Name is required').notEmpty();
+   req.checkBody('email', 'Email is required').notEmpty();
+   req.checkBody('username', 'Username is required').notEmpty();
+   req.checkBody('password', 'Password is required').notEmpty();
+
+   const errors = req.validationErrors();
+
+   if(errors){
+     res.render('register', {errors: errors});
+   } else {
+     console.log('YES');
+   }
+})
+
+
+
 module.exports = router;
